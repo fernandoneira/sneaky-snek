641edcb428a9610a2fb7c7d3bfb324a73283a73f
Fernando Neira <fernando.neira@neonstingray.com>
2016-11-05 16:12:48 +0100

    Replaced angular2 hello world with yeoman-fountain generated starting app.

--- app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'sneaky-snek',
-  template: '<h1>Sneaky Snek is here!</h1>'
-})
-
-export class AppComponent { }
---
+++ src/app/main.js
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'sneaky-snek-app',
+  template: require('./main.html')
+})
+export class MainComponent {}
