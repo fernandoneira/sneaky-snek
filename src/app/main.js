25a295327115817c79c09e217b64ca866a9adb77
Fernando Neira <fernando.neira@neonstingray.com>
2016-11-06 16:41:38 +0100

    Added static wireframe of the game setup.

--- src/app/main.js
+++ src/app/main.js
@@ -1,7 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'sneaky-snek-app',
   template: require('./main.html')
 })
-export class MainComponent {}
+export class MainComponent {
+  @ViewChild("gridCanvas") gridCanvas: ElementRef;
+  gridWidth: number;
+  gridHeight: number;
+  cellSize: number; // width=height, cells are always square
+  horizontalCells: number;
+  verticalCells: number;
+
+  constructor() {
+    // 800/16 = 50x50 cells
+    this.gridWidth = 800;
+    this.gridHeight = 800;
+    this.cellSize = 16;
+    this.horizontalCells = this.gridWidth / this.cellSize;
+    this.verticalCells = this.gridWidth / this.cellSize;
+  }
+
+  ngAfterViewInit() { // wait for the view to init before using the element
+    const canvasContext: CanvasRenderingContext2D = this.gridCanvas.nativeElement.getContext("2d");
+    // happy drawing from here on
+    canvasContext.fillStyle = 'blue';
+    // Sample snake segment
+    canvasContext.fillRect(this.cellSize, this.cellSize, this.cellSize, this.cellSize);
+  }
+}
